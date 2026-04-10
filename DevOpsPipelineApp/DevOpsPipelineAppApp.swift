3e1fe69c8bdfce866d531a6deee577a54ece26ee
AnkithP1199 <ankikth.p@remidio.com>
2026-04-10 11:15:30 +0530

    Initial Commit

---
+++ DevOpsPipelineApp/DevOpsPipelineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevOpsPipelineAppApp.swift
+//  DevOpsPipelineApp
+//
+//  Created by Ankith on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DevOpsPipelineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevOpsPipelineAppTests/DevOpsPipelineAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  DevOpsPipelineAppTests.swift
+//  DevOpsPipelineAppTests
+//
+//  Created by Ankith on 10/04/26.
+//
+
+import Testing
+@testable import DevOpsPipelineApp
+
+struct DevOpsPipelineAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
